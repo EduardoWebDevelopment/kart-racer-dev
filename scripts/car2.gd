7e9eb7bfab4bd863d5e961a28c09420d9b274558
Eduardo <exostar922@gmail.com>
2024-10-04 08:43:37 -0400

    fiddling around

--- scripts/car2.gd
+++ scripts/car2.gd
@@ -1,7 +1,7 @@
 extends VehicleBody3D
 
-var max_rpm = 900
-var max_torque = 600
+var max_rpm = 1000
+var max_torque = 700
 
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5 * delta)
@@ -10,9 +10,3 @@ func _physics_process(delta: float) -> void:
 	$BackLeftWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = $BackRightWheel.get_rpm()
 	$BackRightWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_L:
-			$"".translate(Vector3(0,2,0))
-			print(position)
