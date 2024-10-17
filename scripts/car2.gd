2d5e27a675159a322eabc788828f7e2dc0d2e2a1
Eduardo <exostar922@gmail.com>
2024-10-17 11:30:08 -0400

    trk_2 has collision, skyboxes, velocity, more stuff(?)

--- scripts/car2.gd
+++ scripts/car2.gd
@@ -1,7 +1,9 @@
 extends VehicleBody3D
 
-var max_rpm = 1000
-var max_torque = 600
+var max_rpm = 2500
+var max_torque = 500	
+
+
 
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5 * delta)
@@ -10,3 +12,7 @@ func _physics_process(delta: float) -> void:
 	$BackLeftWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = $BackRightWheel.get_rpm()
 	$BackRightWheel.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
+	
+	var velocity_x = (round(linear_velocity.x * 100) / 100 )
+	var velocity_z = (round(linear_velocity.z * 100) / 100 )
+	print(velocity_x + velocity_z)
