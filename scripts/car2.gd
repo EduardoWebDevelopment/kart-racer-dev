ae5a2f50f58e456a55e9bdb0895d6d75d5aa4fd1
Eduardo <exostar922@gmail.com>
2024-10-29 09:50:40 -0400

    begin the merge process
    YEAH

--- scripts/car2.gd
+++ scripts/car2.gd
@@ -1,9 +1,38 @@
 extends VehicleBody3D
 
+func _ready() -> void:
+	$GameListener.visible = false
+	$Ui.visible = true
+
+
 var max_rpm = 2500
 var max_torque = 500
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pass
+		
+#func pauseMenu():
+	
 
+#func _pause_check():
+	## if pause button is pressed,
+	#if Input.is_action_pressed("pause"):
+		#
+		## if pause menu visiblity is true,
+		#if $GameListener.visible:
+			#
+			## unpause
+			#$GameListener.visible = false
+	#
+	## if pause button is pressed,
+	#if Input.is_action_pressed("pause"):
+		#
+		## if pause menu visiblity is false,
+		#if !$GameListener.visible:
+			#
+			## 
+			#$GameListener.visible = true
 
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5 * delta)
@@ -16,3 +45,7 @@ func _physics_process(delta: float) -> void:
 	var velocity_x = (round(linear_velocity.x * 100) / 100 )
 	var velocity_z = (round(linear_velocity.z * 100) / 100 )
 	print(velocity_x + velocity_z)
+	
+	#_pause_input()
+	
+	
